afc4481f13d4799c428d9e9783d6b3e498cd593e
Marco Garcia <antonio_villegas@hotmail.com>
2020-05-21 20:10:16 -0500

    Initial Commit

---
+++ SwiftUiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiApp
+//
+//  Created by Marco Antonio García Villegas on 21/05/20.
+//  Copyright © 2020 Marco Antonio García Villegas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
